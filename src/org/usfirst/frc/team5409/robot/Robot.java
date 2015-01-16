c2ed791ca537d93e2106e13ebe261e4905313d1c
Zuhair Makda <1makdazuh@hdsb.ca>
2015-01-15 19:45:42 -0500

    changes

--- src/org/usfirst/frc/team5409/robot/Robot.java
+++ src/org/usfirst/frc/team5409/robot/Robot.java
@@ -23,7 +23,7 @@ public class Robot extends IterativeRobot {
 	Talon rightFront;
 	Talon rightBack;
     public void robotInit() {
-    	driver = new Joystick(0);
+    	driver = new Joystick(1);
     	leftFront = new Talon(0);
     	leftBack = new Talon(1);
     	rightFront = new Talon(2);
@@ -40,7 +40,7 @@ public class Robot extends IterativeRobot {
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() {
+    public void teleopPeriodic_old() {
     	float leftStickLeftSideValue = (float)driver.getRawAxis(2);
     	float leftStickRightSideValue = leftStickLeftSideValue;
     	float rightStickLeftSideValue = (float)driver.getRawAxis(3);
@@ -48,26 +48,82 @@ public class Robot extends IterativeRobot {
     	float turn = (float)driver.getRawAxis(0);
     	if(rightStickLeftSideValue > 0){
     		if(turn > 0){ // Turning Right
-    			rightStickRightSideValue -= turn;
+    			rightStickRightSideValue += turn*-1;
     		}else if(turn < 0){ // Turning Left
-    			rightStickLeftSideValue -= turn;
+    			rightStickLeftSideValue += turn;
     		}
 	    		leftFront.set((rightStickLeftSideValue / 3) * -1);
 	            leftBack.set((rightStickLeftSideValue / 3) * -1);
 	            rightFront.set(rightStickRightSideValue / 3);
 	            rightBack.set(rightStickRightSideValue / 3);
-    	}else if(leftStickLeftSideValue > 0){
+    	} else if(leftStickLeftSideValue > 0){
     		if(turn > 0){ // Turning Right
-    			leftStickRightSideValue -= turn;
+    			leftStickRightSideValue += turn*-1;
     		}else if(turn < 0){ // Turning Left
-    			leftStickLeftSideValue -= turn;
+    			leftStickLeftSideValue += turn;
     		}
-            leftFront.set(leftStickLeftSideValue / 3);
-            leftBack.set(leftStickLeftSideValue / 3);
-            rightFront.set((leftStickRightSideValue / 3) * -1);
+            leftFront.set((leftStickLeftSideValue / 3));
+            leftBack.set((leftStickLeftSideValue / 3));
+            rightFront.set((leftStickRightSideValue / 3 * -1));
             rightBack.set((leftStickRightSideValue / 3) * -1);
     	}
-        
+    	else{
+        	leftFront.set(0);
+            leftBack.set(0);
+            rightFront.set(0);
+            rightBack.set(0);
+        }
+    }
+    
+    public void teleopPeriodic() {
+    	float leftStickLeftSideValue = (float)driver.getRawAxis(2);
+    	float leftStickRightSideValue = leftStickLeftSideValue;
+    	float rightStickLeftSideValue = (float)driver.getRawAxis(3);
+    	float rightStickRightSideValue = rightStickLeftSideValue;
+    	float turn = (float)driver.getRawAxis(0);
+    	Movement=GetMovement();
+    	switch(Movement){
+    		case 'Forward':
+    		break;
+    		case 'Reverse':
+    		break;
+    		case 'Right-Forward':
+    		break;
+    		case 'Right-Reverse':
+    		break;
+    		case 'Left-Forward':
+    		break;
+    		case 'Left-Reverse':
+    		break;
+    	
+    	}
+    	if(rightStickLeftSideValue > 0){
+    		if(turn > 0){ // Turning Right
+    			rightStickRightSideValue += turn*-1;
+    		}else if(turn < 0){ // Turning Left
+    			rightStickLeftSideValue += turn;
+    		}
+	    		leftFront.set((rightStickLeftSideValue / 3) * -1);
+	            leftBack.set((rightStickLeftSideValue / 3) * -1);
+	            rightFront.set(rightStickRightSideValue / 3);
+	            rightBack.set(rightStickRightSideValue / 3);
+    	} else if(leftStickLeftSideValue > 0){
+    		if(turn > 0){ // Turning Right
+    			leftStickRightSideValue += turn*-1;
+    		}else if(turn < 0){ // Turning Left
+    			leftStickLeftSideValue += turn;
+    		}
+            leftFront.set((leftStickLeftSideValue / 3));
+            leftBack.set((leftStickLeftSideValue / 3));
+            rightFront.set((leftStickRightSideValue / 3 * -1));
+            rightBack.set((leftStickRightSideValue / 3) * -1);
+    	}
+    	else{
+        	leftFront.set(0);
+            leftBack.set(0);
+            rightFront.set(0);
+            rightBack.set(0);
+        }
     }
     
     /**
